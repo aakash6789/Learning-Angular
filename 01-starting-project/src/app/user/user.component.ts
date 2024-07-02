1dd66e714626e31ec9b1bedf8be0bf5af9e1636f
Aakash <aakash2506c@gmail.com>
2024-07-02 18:12:16 +0530

    ok

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component,signal,Input, input } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+  // @Input() avatar!:string;
+  @Input() name!:string;    
+  avatar=input<string>();
+    selectedUser=signal(DUMMY_USERS[1]);
+    get imagePath(){
+      return 'assets/users/'+this.selectedUser.avatar;
+    }
+    onSelectUser(){
+      const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length);
+      console.log("Loun re");
+      this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    }
 }
